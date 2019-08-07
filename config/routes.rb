a3f1fd9d26e5062e9cecb61d284de6311dff9a13
Bruce <lywctl@gmail.com>
2019-08-07 12:48:44 +0800

    add user sign_up and sign_in api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        collection do
+          post :sign_up
+          post :sign_in
+        end
+      end
+    end
+  end
 end
