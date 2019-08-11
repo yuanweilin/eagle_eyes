26c61ac4664cf5befa5d76fa5aebc8a87d09273c
Bruce <lywctl@gmail.com>
2019-08-11 20:16:19 +0800

    gitignore master.key and credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'https://github.com/yuanweilin/eagle_eyes.git'
 set :deploy_to, '/home/deploy/eagle_eyes'
 set :keep_releases, 5
 
-set :linked_files, %w{config/database.yml config/cable.yml .env}
+set :linked_files, %w{config/database.yml config/cable.yml .env config/master.key config/credentials.yml.enc}
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/uploads')
 
