3b2ebe437bf05c619ccbe5df23641ddf226344d4
Bruce <lywctl@gmail.com>
2019-08-12 21:03:16 +0800

    search api add cache

--- app/controllers/api/v1/querys_controller.rb
+++ app/controllers/api/v1/querys_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::QuerysController < ApplicationController
   def search
     if params[:keyword]
-      articles = PixnetApi.new(params[:keyword]).search
-      articles.map{ |article| PixnetArticle.find_or_create_by!(article.merge(keyword: params[:keyword])) }
-      pixnet_articles = PixnetArticle.search(params[:keyword], params[:page])
+      json = Rails.cache.fetch("pixnet_search_#{params[:keyword]}_#{params[:page]}", expires_in: 1.hours) {
+        articles = PixnetApi.new(params[:keyword]).search
+        articles.map{ |article| PixnetArticle.find_or_create_by!(article.merge(keyword: params[:keyword])) }
+        ActiveModelSerializers::SerializableResource.new(PixnetArticle.search(params[:keyword], params[:page]), {
+          each_serializer: PixnetArticleSerializer
+        }).as_json
+      }
 
-      render json: pixnet_articles
+      render json: json
     else
       head :bad_request
     end
